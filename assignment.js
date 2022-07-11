f19a3ae2279d189747d0b9662aaac8d2926f6987
jipinraj <jipinraj0202@gmail.com>
2022-07-11 23:22:45 +0530

    student object assignment

--- assignment.js
+++ assignment.js
@@ -1,1 +1,6 @@
-console.log("hello")
+const students = [{ name:"Athul ATK",marks:100 , age: 18},
+{name:"Rahuk Giri",marks:22,age:19},
+{name:"Adersh Vijayan",marks:88,age:18}
+]
+let marks= (item) => console.log(`${item.name}-${item.marks}`)
+students.map(marks)
