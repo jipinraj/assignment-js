9f81e568a3b8a9d75873a4f3db55d1045fba9ba6
jipinraj <jipinraj0202@gmail.com>
2022-07-11 23:40:26 +0530

    JSON assignment

--- assignment.js
+++ assignment.js
@@ -4,3 +4,13 @@ const students = [{ name:"Athul ATK",marks:100 , age: 18},
 ]
 let marks= (item) => console.log(`${item.name}-${item.marks}`)
 students.map(marks)
+
+//JSON
+const person = '{"firstName":"Athul", "lastName": "ATK","age":50,"height":182}';
+const obj = JSON.parse(person);
+console.log(obj);
+
+obj.weight = 80;            //adding property
+delete obj.lastName;        //deleting property
+console.log(obj);
+
